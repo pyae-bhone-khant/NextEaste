04f602d8f95ef016e56420c123cfd39d2fa13fb8
pyae-bhone-khant <pyaebhonen4@gmail.com>
2026-09-11 13:08:47 +0630

    fix deploy error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,17 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma"; // your prisma client instance
 
 export const auth = betterAuth({ 
-
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.NEXT_PUBLIC_APP_URL,
+    process.env.BETTER_AUTH_URL,
+    "https://nexteaste.onrender.com",
+  ].filter((origin): origin is string => Boolean(origin)),
+  advanced: {
+    ipAddress: {
+      ipAddressHeaders: ["x-forwarded-for", "x-real-ip"],
+    },
+  },
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "sqlite", ...etc
     }),
